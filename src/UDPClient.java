34f2f38c0ddccf180bfa0cc34a0c6424f269eeb7
Denis NGUYEN <denisnguyen@MacBook-Pro-de-Denis.local>
2024-10-23 15:51:53 +0200

    comments

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -13,23 +13,23 @@ public class UDPClient {
         this.serverPort = port;
     }
 
-    // 
-    public void send() throws Exception {
-        DatagramSocket socket = new DatagramSocket();
-        InetAddress serverInetAddress = InetAddress.getByName(serverHost);
-        Console console = System.console(); // Get a console 
 
-        if (console == null) {
-            System.out.println("Pas de console disponible.");
+    public void send() throws Exception {
+        DatagramSocket socket = new DatagramSocket(); //open a socket 
+        InetAddress serverInetAddress = InetAddress.getByName(serverHost); // get the ip address of a server knowing his name 
+        Console console = System.console(); // get a console 
+
+        if (console == null) { 
+            System.out.println("No console available."); 
             return;
         }
 
         String userInput;
-        userInput = console.readLine("Enter a message: ");
+        userInput = console.readLine("Enter a message: "); // retrieve the user's message
         while (userInput != null) {
             byte[] data = userInput.getBytes("UTF-8");
             DatagramPacket packet = new DatagramPacket(data, data.length, serverInetAddress, serverPort);
-            socket.send(packet); // Envoyer le datagramme
+            socket.send(packet); // send the datagramme
 
             socket.close();
         }
