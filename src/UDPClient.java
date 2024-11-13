98a3305a32a4d815d6d94cd8d2d976f7a6ac46b7
zAlexisD <alexis.doan@ensea.fr>
2024-11-13 15:04:15 +0100

    UDPClient commit 1.1 Modify the while loop

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
 public class UDPClient {
     private String serverHost;
@@ -14,34 +15,39 @@ public class UDPClient {
         this.serverPort = port;
     }
 
-
     public void send() throws Exception {
         DatagramSocket socket = new DatagramSocket(); //open a socket 
         InetAddress serverInetAddress = InetAddress.getByName(serverHost); // get the ip address of a server knowing his name 
-        Console console = System.console(); // get a console 
+        Console console = System.console(); // get a console
 
         if (console == null) { 
-            System.out.println("No console available."); 
-            return;
+            System.err.println("No console available.");
+            System.exit(1);
         }
 
-        String userInput;
-        userInput = console.readLine("Enter a message: "); // retrieve the user's message
-        while (userInput != null) {
-            byte[] data = userInput.getBytes("UTF-8");
+        while (true) {
+            String userInput = console.readLine("Enter a message: "); // retrieve the user's message
+            byte[] data = userInput.getBytes(StandardCharsets.UTF_8);
             DatagramPacket packet = new DatagramPacket(data, data.length, serverInetAddress, serverPort);
-            socket.send(packet); // send datagramme 
+            socket.send(packet); // send datagram
+
+            // Add condition to close the UDP server
+            if (userInput.trim().equalsIgnoreCase("exit console")){
+                System.out.println("Closing console...\n");
+                break;
+            }
         }
         socket.close();
     }
     public static void main(String[] args) throws Exception {
         if (args.length < 2) { // check the arguments
-            System.out.println("Usage: java UDPClient <host> <port>");
-            return;
+            System.err.println("Usage: java UDPClient <host> <port>");
+            System.exit(1);
         }
 
         String host = args[0];
         int port = Integer.parseInt(args[1]);
+
         UDPClient client = new UDPClient(host, port);
         client.send();
     }
